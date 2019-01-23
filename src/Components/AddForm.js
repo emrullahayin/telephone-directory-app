3e9f07aef5e4bc2e5ab18540578c279fdd4b3654
Emrullah <emrullahayin@gmail.com>
2019-01-24 00:47:30 +0300

    import name update

--- src/Components/AddForm.js
+++ src/Components/AddForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'clasllahsnames';
+import classNames from 'classnames';
 import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
