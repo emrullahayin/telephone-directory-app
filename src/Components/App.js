387a393c4c788524bd70ea7090eb0b3097eb3ae5
Emrullah Ayın <e.ayin@jollytur.com>
2019-03-18 11:53:38 +0300

    .

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,47 +4,47 @@ import './App.css';
 import Contacts from './Contacts';
 
 class App extends Component {
-	state = {
-		data: [
-			{
-				id: 0,
-				name: 'Emrullah',
-				number: '+90 553 620 34 52'
-			},
-			{
-				id: 1,
-				name: 'Mustafa',
-				number: '+90 541 389 00 00'
-			},
-			{
-				id: 2,
-				name: 'Ali',
-				number: '+90 532 412 52 52'
-			}
-		]
-	};
+  state = {
+    data: [
+      {
+        id: 0,
+        name: 'Emrullah',
+        number: '+90 553 620 34 52'
+      },
+      {
+        id: 1,
+        name: 'Mustafa',
+        number: '+90 541 389 00 00'
+      },
+      {
+        id: 2,
+        name: 'Ali',
+        number: '+90 532 412 52 52'
+      }
+    ]
+  };
 
-	addContact = (contact) => {
-		const { data } = this.state;
-		data.push(contact);
-		this.setState({ data });
-	}
-	deleteContact = (id) => {
-		const { data } = this.state;
-		delete data[id];
-		this.setState({ data });
-	}
-	render() {
-		return (
-			<div className="App">
-				<Contacts
-					contacts={this.state.data}
-					addContact={this.addContact}
-					deleteContact={this.deleteContact}
-				/>
-			</div>
-		);
-	}
+  addContact = (contact) => {
+    const { data } = this.state;
+    data.push(contact);
+    this.setState({ data });
+  }
+  deleteContact = (id) => {
+    const { data } = this.state;
+    delete data[id];
+    this.setState({ data });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Contacts
+          contacts={this.state.data}
+          addContact={this.addContact}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
